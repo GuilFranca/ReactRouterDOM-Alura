8fc8f9db92176b168f62788963e8e9567afc457d
Guilherme França <guilfranca27@gmail.com>
2026-05-21 23:13:12 -0300

    Aula 1 06 Criando o roteador -> finalizado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AboutMe from "./pages/aboutMe";
 import Home from "./pages/home";
 
 function App() {
   return (
     <BrowserRouter>
-    
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="sobre-mim" element={<AboutMe />} />
+        <Route path="*" element={<div>Página não encontrada</div>} />
+      </Routes>
     </BrowserRouter>
   );
 }
