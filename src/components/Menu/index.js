14b42e2106a0d3128080c2890541166d422a360c
Guilherme França <guilfranca27@gmail.com>
2026-05-24 16:56:18 -0300

    Aula 2 - 02 Links do react-router-dom -> Finalizado

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,3 +1,17 @@
+import { Link } from "react-router-dom"
+import styles from "./Menu.module.css"
+
 export default function Menu() {
-    return "Menu de navegação"
+    return (
+        <header>
+            <nav className={styles.navegation}>
+                <Link className={styles.link} to="/">
+                    Início
+                </Link>
+                <Link className={styles.link} to="/sobre-mim">
+                    Sobre mim
+                </Link>
+            </nav>
+        </header>
+    )
 }
