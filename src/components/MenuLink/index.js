ff4ebe87244da91f4f4cccfa6d54fc1c1c998462
Guilherme França <guilfranca27@gmail.com>
2026-05-26 19:10:32 -0300

    Aula 2 - 07 Desafio: utilizando NavLink -> Finalizado

--- src/components/MenuLink/index.js
+++ src/components/MenuLink/index.js
@@ -1,13 +1,12 @@
-import { Link, useLocation } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import styles from "./MenuLink.module.css"
 
 
 export default function MenuLink({ children, to }) {
-    const roteLocate = useLocation();
 
     return (
-        <Link className={`${styles.link} ${roteLocate.pathname === to ? styles.linkSelect : null}`} to={to}>
+        <NavLink className={({ isActive }) => ` ${styles.link} ${isActive ? styles.linkSelect : ""}`} to={to}>
             {children}
-        </Link>
+        </NavLink>
     )
 }
